1c1c2dccb5e8764879cc0cb2016d9a6502e40414
Kamala16 <olach16@gmail.com>
2019-05-28 11:19:57 +0200

    Dodany plik Graf.cpp

---
+++ Graf.cpp
@@ -0,0 +1,17 @@
+#include "Graf.hpp"
+
+void Graf::krytycznaSciezka(PrzydzialZasobow &przydzial) {
+    
+}
+
+void Graf::najdrozszaSciezka(PrzydzialZasobow &przydzial) {
+
+}
+
+void Graf::najdrozszeTK(PrzydzialZasobow &przydzial) {
+
+}
+
+void Graf::najmniejObciazony(PrzydzialZasobow &przydzial) {
+
+}
